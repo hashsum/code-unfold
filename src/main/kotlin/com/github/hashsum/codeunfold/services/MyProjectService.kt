6f386d7cca684171858e543417cf4cc152bb1181
GitHub Action <action@github.com>
2022-10-18 16:21:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/hashsum/codeunfold/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hashsum/codeunfold/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.hashsum.codeunfold.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hashsum.codeunfold.MyBundle
 
 class MyProjectService(project: Project) {
 
